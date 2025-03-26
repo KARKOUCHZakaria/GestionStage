124432aa373bc1f94653c7c2fecc3cee270ba401
KARKOUCH Zakaria <164746166+KARKOUCHZakaria@users.noreply.github.com>
2025-03-26 10:55:24 +0000

    first commit

---
+++ src/main/java/ma/emsi/Gestion_De_Stage/GestionDeStageApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.Gestion_De_Stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeStageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/emsi/Gestion_De_Stage/GestionDeStageApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.Gestion_De_Stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
